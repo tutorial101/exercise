692bb1352ead980d039307b0dfcbdf3950d9bdcc
tutorial101 <tutorial101@example.com>
2016-05-16 21:04:26 +0900

    Add loop version of fib

--- src/exercise/Recursive3.java
+++ src/exercise/Recursive3.java
@@ -11,9 +11,25 @@ public class Recursive3 {
         }
     }
     
+    /** Loop **/
+    public int fib2(int n) {
+        int x = 0;
+        int y = 1;
+        for (int i = 0; i < n; i++) {
+            int _x = x; int _y = y;
+            x = _y;
+            y = _x + _y;
+            // or
+            //int tmp = y; y = x + y; x = tmp;
+        }
+        return x;
+    }
+    
     public static void main(String[] args) {
         int result = new Recursive3().fib(10);
         System.out.println(result);   //=> 55
+        int result2 = new Recursive3().fib2(10);
+        System.out.println(result2);
     }
 
 }
